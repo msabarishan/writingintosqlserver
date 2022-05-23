7121789a53699dc15eeceebd8906b2fe57d0f070
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-05-23 20:03:04 +0530

    Update to_sql.py

--- to_sql.py
+++ to_sql.py
@@ -7,10 +7,10 @@ import postgreconnect
 from altair import datum
 
 #Define a sql to fetch data from CARS table (PostgreSQL table hosted on Heroku)
-sql='INSERT INTO COMPANY VALUES(13,'SAFARAZ',27,'NORTH DAKOTA',3000);'
+
 company_data='Select * from company;'
 #postgreconnect.runquery function is called by passing on the sql. The function runquery returns the data from the CARS table.
-postgreconnect.runquery(sql)
+
 #The returned data is converted into a DataFrame.
 sql_data=pd.DataFrame(postgreconnect.runquery(company_data))
 
