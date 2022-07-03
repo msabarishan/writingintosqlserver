f7ab69e38c5d7b17ea97eb3a48730f99de4ed97f
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-07-03 13:56:21 +0530

    Update codes.py

--- codes.py
+++ codes.py
@@ -6,7 +6,7 @@ import streamlit as st
 # Read Excel File
 st.subheader('Upload Input Files')
 excel_ip= st.file_uploader("Choose a Machine Priority CSV file")
-option = st.selectbox('Select the  requirement?',('Add', 'Delete', 'Update'))
+option = st.selectbox('Select the  requirement?',('','Add', 'Delete', 'Update'))
 st.write('You selected:', option)
 try:
      if excel_ip is not None:
