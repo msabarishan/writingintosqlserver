e9e2232918982dc777ee312777314ded2f5e846d
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-07-03 11:29:04 +0530

    Update codes.py

--- codes.py
+++ codes.py
@@ -6,54 +6,57 @@ import streamlit as st
 # Read Excel File
 st.subheader('Upload Input Files')
 excel_ip= st.file_uploader("Choose a Machine Priority CSV file")
-
-if excel_ip is not None:
-           df = pd.read_csv(excel_ip) #mp-machine priority data frame
-insert_values = df.values.tolist()
+try:
+     if excel_ip is not None:
+                df = pd.read_csv(excel_ip) #mp-machine priority data frame
+     insert_values = df.values.tolist()
 
 # Use your credentials
-hostname = 'ec2-34-207-12-160.compute-1.amazonaws.com'
-database = 'd5fmmj82e013ta'
-username = 'gpwvcgfprcaaji'
-pwd = 'abf0e72c574eebd3810c44df8d690a1398dc1e551ff6ee306295825801cb39fe'
-port_id = 5432
-conn = None
+     hostname = 'ec2-34-207-12-160.compute-1.amazonaws.com'
+     database = 'd5fmmj82e013ta'
+     username = 'gpwvcgfprcaaji'
+     pwd = 'abf0e72c574eebd3810c44df8d690a1398dc1e551ff6ee306295825801cb39fe'
+     port_id = 5432
+     conn = None
 
-try:
-    with psycopg2.connect(
-                host = hostname,
-                dbname = database,
-                user = username,
-                password = pwd,
-                port = port_id) as conn:
+     try:
+         with psycopg2.connect(
+                     host = hostname,
+                     dbname = database,
+                     user = username,
+                     password = pwd,
+                     port = port_id) as conn:
 
-        with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
+             with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
 # Dropping existing table
-            cur.execute('DROP TABLE IF EXISTS employees')
+                 cur.execute('DROP TABLE IF EXISTS employees')
 # Create new table
-            create_script = ''' CREATE TABLE IF NOT EXISTS employees (
+                 create_script = ''' CREATE TABLE IF NOT EXISTS employees (
                                     id      int PRIMARY KEY,
                                     name    varchar(40) NOT NULL,
                                     salary  int,
                                     dept_id varchar(30)) '''
-            cur.execute(create_script)
+                 cur.execute(create_script)
 # List created from input data are passed
-            insert_script  = 'INSERT INTO employees (id, name, salary, dept_id) VALUES (%s, %s, %s, %s)'
-            for record in insert_values:
+                 insert_script  = 'INSERT INTO employees (id, name, salary, dept_id) VALUES (%s, %s, %s, %s)'
+                 for record in insert_values:
                 cur.execute(insert_script, record)
 # For Updating salary with 50% hike
-            update_script = 'UPDATE employees SET salary = salary + (salary * 0.5)'
-            cur.execute(update_script)
+                 update_script = 'UPDATE employees SET salary = salary + (salary * 0.5)'
+                 cur.execute(update_script)
 # Deleting records
-            delete_script = 'DELETE FROM employees WHERE name = %s'
-            delete_record = ('James',)
-            cur.execute(delete_script, delete_record)
+                 delete_script = 'DELETE FROM employees WHERE name = %s'
+                 delete_record = ('James',)
+                 cur.execute(delete_script, delete_record)
 
-            cur.execute('SELECT * FROM EMPLOYEES')
-            for record in cur.fetchall():
-                print(record['name'], record['salary'])
-except Exception as error:
-    print(error)
-finally:
-    if conn is not None:
-      conn.close()
+                 cur.execute('SELECT * FROM EMPLOYEES')
+                 for record in cur.fetchall():
+                     print(record['name'], record['salary'])
+     except Exception as error:
+         print(error)
+
+     finally:
+         if conn is not None:
+            conn.close()
+except:
+    pass
