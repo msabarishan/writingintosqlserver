6d4ff89ce529b95aede5df9cb5ed51b9c8ba11cc
msabarishan <103972490+msabarishan@users.noreply.github.com>
2022-07-10 18:36:30 +0530

    Update codes.py

--- codes.py
+++ codes.py
@@ -32,13 +32,62 @@ try:
                          
 # Dropping existing table
                        cur.execute('DROP TABLE IF EXISTS employees')
+                       cur.execute('DROP TABLE IF EXISTS client')
+                       cur.execute('DROP TABLE IF EXISTS works_with')
+                       cur.execute('DROP TABLE IF EXISTS branch_supplier')
+                       cur.execute('DROP TABLE IF EXISTS branch')
+                    
+                    
 # Create new table
                        create_script = ''' CREATE TABLE IF NOT EXISTS employees (
                                           id      int PRIMARY KEY,
                                           name    varchar(40) NOT NULL,
                                           salary  int,
-                                          dept_id varchar(30)) '''
+                                          dept_id varchar(30)) '''        
                        cur.execute(create_script)
+                       create_script1 = ''' CREATE TABLE branch (
+                                            branch_id INT PRIMARY KEY,
+                                            branch_name VARCHAR(40),
+                                            mgr_id INT,
+                                            mgr_start_date DATE,
+                                            FOREIGN KEY(mgr_id) REFERENCES employee(emp_id) ON DELETE SET NULL
+                                          ) '''        
+                       cur.execute(create_script1)
+                       create_script2 = ''' ALTER TABLE employee
+                                          ADD FOREIGN KEY(branch_id)
+                                          REFERENCES branch(branch_id)
+                                          ON DELETE SET NULL '''        
+                       cur.execute(create_script2)
+                       create_script3 = ''' 
+                                          ALTER TABLE employee
+                                          ADD FOREIGN KEY(super_id)
+                                          REFERENCES employee(emp_id)
+                                          ON DELETE SET NULL; '''        
+                       cur.execute(create_script3)
+                       create_script4 = ''' CREATE TABLE client (
+                                            client_id INT PRIMARY KEY,
+                                            client_name VARCHAR(40),
+                                            branch_id INT,
+                                            FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE SET NULL
+                                          ) '''        
+                       cur.execute(create_script4)
+                       create_script5 = ''' CREATE TABLE works_with (
+                                            emp_id INT,
+                                            client_id INT,
+                                            total_sales INT,
+                                            PRIMARY KEY(emp_id, client_id),
+                                            FOREIGN KEY(emp_id) REFERENCES employee(emp_id) ON DELETE CASCADE,
+                                            FOREIGN KEY(client_id) REFERENCES client(client_id) ON DELETE CASCADE
+                                          )) '''        
+                       cur.execute(create_script5)
+                       create_script6 = ''' CREATE TABLE branch_supplier (
+                                            branch_id INT,
+                                            supplier_name VARCHAR(40),
+                                            supply_type VARCHAR(40),
+                                            PRIMARY KEY(branch_id, supplier_name),
+                                            FOREIGN KEY(branch_id) REFERENCES branch(branch_id) ON DELETE CASCADE
+                                          ) '''        
+                       cur.execute(create_script6)
                        st.write('Reset Successfully')
 # List created from input data are passed
                   elif(option=='Add'):     
